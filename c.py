f803af87d7917062f85a4b346a4c0a2e35fefc41
Sreekar <Sreekar.Eajarla@bilvantis.io>
2025-02-06 22:59:43 +0530

    added c.py

--- c.py
+++ c.py
@@ -1,6 +1,7 @@
 # Created by: Sreekar
 # Created date: 2025-02-06 22:57:47
-# Version: 10.0
+# Version: 10.1
 # Last Modified by: Sreekar
-# Last Modified date: 2025-02-06 22:59:16
+# Last Modified date: 2025-02-06 22:59:43
  
+print("Successful")
