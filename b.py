937be9fd0fb4bd50ba8bbc780b5ddd053e8c71b3
Sreekar <Sreekar.Eajarla@bilvantis.io>
2025-02-06 22:52:17 +0530

    added b.py

--- b.py
+++ b.py
@@ -1,10 +1,11 @@
 # Created by: Sreekar
 # Created date: 2025-02-06 22:51:19
-# Version: 1.2
+# Version: 1.3
 # Last Modified by: Sreekar
-# Last Modified date: 2025-02-06 22:52:03
+# Last Modified date: 2025-02-06 22:52:17
  
 a = 2
 b = 3
 
 print(a+b)
+
