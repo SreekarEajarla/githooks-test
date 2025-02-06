190db1b8727cafd75e0974730ff8300a3c6ba483
Sreekar <Sreekar.Eajarla@bilvantis.io>
2025-02-06 18:02:57 +0530

    sd

--- test.py
+++ test.py
@@ -2,12 +2,10 @@
  * Created by: Sreekar
  * Created date: 2025-02-06 17:20:19
  * Last Modified by: Sreekar
- * Last Modified date: 2025-02-06 17:32:10
+ * Last Modified date: 2025-02-06 18:02:58
  */
 
 
 print("Hello World")
-
-print("asd")
 
 print("Sruthi")
