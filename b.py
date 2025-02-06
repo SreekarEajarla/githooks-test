1b12e9311400dd5b711c08326b8fa8b4f7a2ebf7
Sreekar <Sreekar.Eajarla@bilvantis.io>
2025-02-06 22:51:19 +0530

    added b.py

--- b.py
+++ b.py
@@ -1,7 +1,7 @@
 # Created by: Sreekar
-# Created date: 2025-02-06 22:38:31
-# Version: 1.0.1
+# Created date: 2025-02-06 22:51:19
+# Version: 1.0
 # Last Modified by: Sreekar
-# Last Modified date: 2025-02-06 22:41:09
+# Last Modified date: 2025-02-06 22:51:19
  
-a = 3
+a = 2
--- setup-hooks.sh
+++ setup-hooks.sh
@@ -1,12 +1,22 @@
 #!/bin/sh
 
-# Check if the symbolic link already exists
-if [ -L .git/hooks/pre-commit ]; then
-    echo "Symbolic link already exists. Removing the existing link."
+# # Check if the symbolic link already exists
+# if [ -L .git/hooks/pre-commit ]; then
+#     echo "Symbolic link already exists. Removing the existing link."
+#     rm -rf .git/hooks/pre-commit
+# elif [ -e .git/hooks/pre-commit ]; then
+#     echo "A file named 'pre-commit' already exists in .git/hooks. Please remove it manually."
+#     exit 1
+# fi
+
+# # Create the symbolic link
+# ln -s ../../scripts/hooks/pre-commit .git/hooks/pre-commit
+# echo "Symbolic link created successfully."
+
+# Check if the pre-commit file or symbolic link already exists
+if [ -e .git/hooks/pre-commit ]; then
+    echo "Removing the existing pre-commit hook."
     rm -rf .git/hooks/pre-commit
-elif [ -e .git/hooks/pre-commit ]; then
-    echo "A file named 'pre-commit' already exists in .git/hooks. Please remove it manually."
-    exit 1
 fi
 
 # Create the symbolic link
