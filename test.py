d3c3a0e761a47893b0ae850f238adf4de8250665
Sreekar <Sreekar.Eajarla@bilvantis.io>
2025-02-06 17:32:09 +0530

    Added helloworld code

--- test.py
+++ test.py
@@ -2,8 +2,10 @@
  * Created by: Sreekar
  * Created date: 2025-02-06 17:20:19
  * Last Modified by: Sreekar
- * Last Modified date: 2025-02-06 17:20:19
+ * Last Modified date: 2025-02-06 17:32:10
  */
 
+
+print("Hello World")
 
 print("asd")
