5b1e06e3cfac154977180c01107d55a9ffc23506
Sruthi179 <sruthi.eluru@bilvantis.io>
2025-02-06 19:18:16 +0530

    asdfsaxz

--- a.py
+++ a.py
@@ -1,5 +1,7 @@
 # Created by: Sruthi179
 # Created date: 2025-02-06 18:44:50
 # Last Modified by: Sruthi179
-# Last Modified date: 2025-02-06 18:44:50
+# Last Modified date: 2025-02-06 19:18:16
+
+
 print("Sruthi")
--- a.sql
+++ a.sql
@@ -1,7 +1,9 @@
 # Created by: Sreekar
 # Created date: 2025-02-06 19:12:12
-# Last Modified by: Sreekar
-# Last Modified date: 2025-02-06 19:12:12
+# Last Modified by: Sruthi179
+# Last Modified date: 2025-02-06 19:18:16
+
+
 
 
 select * from table;
