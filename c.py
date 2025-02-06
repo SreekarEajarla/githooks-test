b56ce7a88d4c1c8ddd64555553d7547f84e29b0d
Sreekar <Sreekar.Eajarla@bilvantis.io>
2025-02-06 22:58:18 +0530

    added c.py

--- c.py
+++ c.py
@@ -1,6 +1,6 @@
 # Created by: Sreekar
 # Created date: 2025-02-06 22:57:47
-# Version: 2.0
+# Version: 2.6
 # Last Modified by: Sreekar
-# Last Modified date: 2025-02-06 22:58:03
+# Last Modified date: 2025-02-06 22:58:19
  
