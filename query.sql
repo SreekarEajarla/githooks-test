046f5d83028f35ec105c7287d308cedd7766f626
Sruthi179 <sruthi.eluru@bilvantis.io>
2025-02-07 17:50:53 +0530

    Updated codes

--- query.sql
+++ query.sql
@@ -2,8 +2,7 @@
 -- creation time: 2025-02-07 17:48:03
 -- Modified by            Version          Modified timestamp         Commit message
 -- --------------------------------------------------------------------------------
+-- Sruthi179            1.1             2025-02-07 17:50:54     Updated files
 -- Sruthi179            1.1             2025-02-07 17:49:27     Updated code
 -- Sreekar              1.0             2025-02-07 17:48:03     Updated test files
-
-
 
