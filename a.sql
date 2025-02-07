8cfe7026f537a7aa97fd619c9c804efdee1aa5e9
Sruthi179 <sruthi.eluru@bilvantis.io>
2025-02-07 16:32:03 +0530

    sql file added

---
+++ a.sql
@@ -0,0 +1,6 @@
+# creator name: Sruthi179
+# creation time: 2025-02-07 16:32:04
+# Modified by            Version          Modified timestamp         Commit message
+# --------------------------------------------------------------------------------
+# Sruthi179            1.0             2025-02-07 16:32:04     
+
