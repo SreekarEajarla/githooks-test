e5acf56b5e2e05e898c8e4efc95a6899814cfe8f
Sreekar <Sreekar.Eajarla@bilvantis.io>
2025-02-07 16:13:16 +0530

    added g.py

---
+++ g.py
@@ -0,0 +1,6 @@
+#creator name: Sreekar
+#creation time: 2025-02-07 16:13:16
+#Modified by            Version          Modified timestamp         Commit message
+#--------------------------------------------------------------------------------
+#Sreekar              1.0             2025-02-07 16:13:16     added new sql file
+
