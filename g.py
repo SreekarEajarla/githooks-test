ea676c53a648b549c0d25a2a93557e90df1a4b31
Sruthi179 <sruthi.eluru@bilvantis.io>
2025-02-07 16:20:24 +0530

    code added

--- g.py
+++ g.py
@@ -1,6 +1,13 @@
+# Created by: Sruthi179
+# Created date: 2025-02-07 16:20:24
+# Version: 1.0
+# Last Modified by: Sruthi179
+# Last Modified date: 2025-02-07 16:20:24
+ 
 #creator name: Sreekar
 #creation time: 2025-02-07 16:13:16
 #Modified by            Version          Modified timestamp         Commit message
 #--------------------------------------------------------------------------------
 #Sreekar              1.0             2025-02-07 16:13:16     added new sql file
 
+print("a")
