1d6c88a093bc83c6073bfb5583b22ecf494652df
Sreekar <Sreekar.Eajarla@bilvantis.io>
2025-02-07 15:48:23 +0530

    added new sql file

---
+++ a.sql
@@ -0,0 +1,6 @@
+creator name: Sreekar
+creation time: 2025-02-07 15:48:24
+Modified by            Version          Modified timestamp         Commit message
+--------------------------------------------------------------------------------
+Sreekar              1.0             2025-02-07 15:48:24     git hooks
+
