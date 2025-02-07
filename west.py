8fe4ed4a2056d54e3d2ab02ff710f0cdb158e148
Sruthi179 <sruthi.eluru@bilvantis.io>
2025-02-07 17:42:36 +0530

    Updated code

--- west.py
+++ west.py
@@ -2,8 +2,7 @@
 # creation time: 2025-02-07 17:31:19
 # Modified by            Version          Modified timestamp         Commit message
 # --------------------------------------------------------------------------------
+# Sruthi179            1.1             2025-02-07 17:42:37     sql file added
 # Sreekar              1.1             2025-02-07 17:40:38     Added test files
 # Sreekar              1.0             2025-02-07 17:31:19     added test
-
-
 
