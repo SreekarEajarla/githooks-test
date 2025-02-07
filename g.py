0061f7202c323a74ca068f21c02a7ca86fbfeac5
Sruthi179 <sruthi.eluru@bilvantis.io>
2025-02-07 16:20:55 +0530

    code added

--- g.py
+++ g.py
@@ -1,9 +1,3 @@
-# Created by: Sruthi179
-# Created date: 2025-02-07 16:20:24
-# Version: 1.0
-# Last Modified by: Sruthi179
-# Last Modified date: 2025-02-07 16:20:24
- 
 #creator name: Sreekar
 #creation time: 2025-02-07 16:13:16
 #Modified by            Version          Modified timestamp         Commit message
