8b930a4995cfdac3e87d3957dc86cbf3b7f9e926
Sreekar <Sreekar.Eajarla@bilvantis.io>
2025-02-09 18:49:49 +0530

    COde

--- b.sql
+++ b.sql
@@ -4,3 +4,9 @@
 -- --------------------------------------------------------------------------------
 -- Sreekar              1.0             2025-02-09 18:48:48     Hook updated
 
+
+
+-- Create a table to store customer information
+CREATE TABLE customers (
+    customer_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,        -- Sreekar              1.0             2025-02-09 18:49:49     New
