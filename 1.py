2d4288f6b8897e92fc2ec2d4cf1272e6461fddaf
Sreekar <Sreekar.Eajarla@bilvantis.io>
2025-02-09 19:16:03 +0530

    Updated sh

---
+++ 1.py
@@ -0,0 +1,6 @@
+# creator name: Sreekar
+# creation time: 2025-02-09 19:16:03
+# Modified by            Version          Modified timestamp         Commit message
+# --------------------------------------------------------------------------------
+# Sreekar              1.0             2025-02-09 19:16:03     
+
--- b.sql
+++
@@ -1,8 +0,0 @@
--- creator name: Sreekar
--- creation time: 2025-02-09 19:03:10
--- Modified by            Version          Modified timestamp         Commit message
--- --------------------------------------------------------------------------------
--- Sreekar              1.2             2025-02-09 19:05:59     Hookkkk updated
--- Sreekar              1.1             2025-02-09 19:03:31     Hookkkk updates
--- Sreekar              1.0             2025-02-09 19:03:10     File chnageedddddddd
-
