2a173e6e0ee7f7ed37832e2c0effcf9b2f3129bb
Sruthi179 <sruthi.eluru@bilvantis.io>
2025-02-10 16:48:49 +0530

    print added

--- r.py
+++ r.py
@@ -2,8 +2,10 @@
 # creation time: 2025-02-10 16:12:51
 # Modified by            Version          Modified timestamp         Commit message
 # --------------------------------------------------------------------------------
+# Sruthi179            1.1             2025-02-10 16:48:49     Updated codes
 # Sreekar              1.1             2025-02-10 16:13:21     r.py code changes
 # Sreekar              1.0             2025-02-10 16:12:51     r.py added
 
 
 
+print("Sruthi")
