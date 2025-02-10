91eb40109a68b2a6f1e9e7c7d7ba92485b2f59b2
Sreekar <Sreekar.Eajarla@bilvantis.io>
2025-02-10 15:11:47 +0530

    Added a.py

---
+++ a.py
@@ -0,0 +1,10 @@
+# creator name: Sreekar
+# creation time: 2025-02-10 15:11:48
+# Modified by            Version          Modified timestamp         Commit message
+# --------------------------------------------------------------------------------
+# Sreekar              1.4             2025-02-10 15:12:02     PENDING_COMMIT_MESSAGE
+# Sreekar              1.3             2025-02-10 15:11:58     Added a.py
+# Sreekar              1.2             2025-02-10 15:11:54     Added a.py
+# Sreekar              1.1             2025-02-10 15:11:50     Added a.py
+# Sreekar              1.0             2025-02-10 15:11:48     Added a.py
+
--- setup-hooks.sh
+++ setup-hooks.sh
@@ -6,4 +6,12 @@ if [ -e .git/hooks/pre-commit ]; then
 fi
 
 ln -s ../../scripts/hooks/pre-commit .git/hooks/pre-commit
-echo "Symbolic link created successfully."
+echo "Symbolic link created successfully for pre-commit."
+
+if [ -e .git/hooks/post-commit ]; then
+    echo "Removing the existing post-commit hook."
+    rm -rf .git/hooks/post-commit
+fi
+
+ln -s ../../scripts/hooks/post-commit .git/hooks/post-commit
+echo "Symbolic link created successfully for post-commit."
