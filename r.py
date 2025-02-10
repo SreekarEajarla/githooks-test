7068954c3f5ff54cdd2de2d639c0f3ab9fd8736c
Sreekar <Sreekar.Eajarla@bilvantis.io>
2025-02-10 16:12:51 +0530

    r.py added

---
+++ r.py
@@ -0,0 +1,6 @@
+# creator name: Sreekar
+# creation time: 2025-02-10 16:12:51
+# Modified by            Version          Modified timestamp         Commit message
+# --------------------------------------------------------------------------------
+# Sreekar              1.0             2025-02-10 16:12:51     r.py added
+
