d075e8582c56447c248233bc8cbcdb5e5ca9fd88
Sreekar <Sreekar.Eajarla@bilvantis.io>
2025-02-10 16:56:58 +0530

    x.py added

---
+++ x.py
@@ -0,0 +1,6 @@
+# creator name: Sreekar
+# creation time: 2025-02-10 16:56:59
+# Modified by            Version          Modified timestamp         Commit message
+# --------------------------------------------------------------------------------
+# Sreekar              1.0             2025-02-10 16:56:59     x.py added
+
