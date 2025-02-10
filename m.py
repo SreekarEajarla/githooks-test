2d82419fafeea393350ccdd508e44b431120e0e1
Sruthi179 <sruthi.eluru@bilvantis.io>
2025-02-10 17:02:18 +0530

    b defined

--- m.py
+++ m.py
@@ -2,6 +2,7 @@
 # creation time: 2025-02-10 16:57:53
 # Modified by         Version          Modified timestamp      Commit message
 # --------------------------------------------------------------------------------
+# Sruthi179            1.5             2025-02-10 17:02:18     b defined
 # Sruthi179            1.4             2025-02-10 17:01:57     a defined
 # Sruthi179            1.3             2025-02-10 17:01:22     print removed
 # Sreekar              1.2             2025-02-10 16:58:47     print 2
@@ -16,3 +17,4 @@ print("Hello World")
 
 
 a = 2
+b = 1
