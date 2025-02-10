70bfe7ba0cffed64ba9ca29e91fccf497192b9f5
Sruthi179 <sruthi.eluru@bilvantis.io>
2025-02-10 16:49:42 +0530

    Changes by sruthi

--- r.py
+++ r.py
@@ -2,7 +2,7 @@
 # creation time: 2025-02-10 16:12:51
 # Modified by            Version          Modified timestamp         Commit message
 # --------------------------------------------------------------------------------
-# Sruthi179            1.1             2025-02-10 16:48:49     Updated codes
+# Sruthi179            1.2             2025-02-10 16:49:42     Changes by sruthi
 # Sreekar              1.1             2025-02-10 16:13:21     r.py code changes
 # Sreekar              1.0             2025-02-10 16:12:51     r.py added
 
