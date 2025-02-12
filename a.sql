f3c793c09fb936bcc2d2a0343a891b4273a9efa1
Sruthi179 <sruthi.eluru@bilvantis.io>
2025-02-12 14:19:16 +0530

    qwertyuio

--- a.sql
+++ a.sql
@@ -3,10 +3,10 @@
  Modified by         Version          Modified timestamp      Commit message
  --------------------------------------------------------------------------------
  Sruthi179            1.0             2025-02-12 12:39:00     an sql file
+ printf ' %-20s %-15s %-23s %s
+' "Sruthi179" "1.0" "2025-02-12 14:19:16" "qwertyuio"
 */
 
 
 
 select b from a;
-select x from y;
-select ab from x;g
