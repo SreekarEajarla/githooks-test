0e269292f38cac21eea464fb16d056bf223cee7f
Sruthi179 <sruthi.eluru@bilvantis.io>
2025-02-12 15:11:04 +0530

    sal from b

--- s.sql
+++ s.sql
@@ -5,7 +5,8 @@
  Sruthi179            1.0             2025-02-12 15:01:56     qwerty
  Sruthi179            1.0             2025-02-12 15:02:29     * to sal
  Sruthi179            1.0             2025-02-12 15:10:38     * from b
+ Sruthi179            1.0             2025-02-12 15:11:04     sal from b
 */
 
 select sal from a;
-select * from b;
+select sal from b;
