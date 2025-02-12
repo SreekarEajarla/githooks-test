c7e3ffbeac737bc214cf04bf0f5c12341df0c54b
Sruthi179 <sruthi.eluru@bilvantis.io>
2025-02-12 15:23:42 +0530

    * from b

--- s.sql
+++ s.sql
@@ -3,9 +3,11 @@ creator name: Sruthi179
 creation time: 2025-02-12 15:22:49
 Modified by         Version          Modified timestamp      Commit message
 --------------------------------------------------------------------------------
+Sruthi179            1.1             2025-02-12 15:23:42     * from b
 Sruthi179            1.0             2025-02-12 15:22:49     aszdfghjkl
 */
 
 
+
 select sal from a;
-select sal from b;
+select * from b;
