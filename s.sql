1103063fb82070e16492a53822aaa1a38655dddb
Sruthi179 <sruthi.eluru@bilvantis.io>
2025-02-12 15:02:29 +0530

    * to sal

--- s.sql
+++ s.sql
@@ -3,6 +3,7 @@
  Modified by         Version          Modified timestamp      Commit message
  --------------------------------------------------------------------------------
  Sruthi179            1.0             2025-02-12 15:01:56     qwerty
+ Sruthi179            1.0             2025-02-12 15:02:29     * to sal
 */
 
-select * from a;
+select sal from a;
