844f11469733aa28a14642f9fe2a9eec7b8aaae8
Sruthi179 <sruthi.eluru@bilvantis.io>
2025-02-12 12:23:15 +0530

    New file created

---
+++ b.sql
@@ -0,0 +1,8 @@
+/* creator name: Sruthi179
+ creation time: 2025-02-12 12:23:15
+ Modified by         Version          Modified timestamp      Commit message
+ --------------------------------------------------------------------------------
+ Sruthi179            1.0             2025-02-12 12:23:15     New file created
+*/
+
+select * from table;
