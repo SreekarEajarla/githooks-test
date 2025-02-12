d0d6b7c07f19b5eb25b51955240a8bdca7e6453c
Sruthi179 <sruthi.eluru@bilvantis.io>
2025-02-12 14:50:14 +0530

    removed two line

--- a.sql
+++ a.sql
@@ -8,10 +8,10 @@
  Sruthi179            1.0             2025-02-12 14:47:36     asdfg
  Sruthi179            1.0             2025-02-12 14:47:57     qwerty
  Sruthi179            1.1             2025-02-12 14:49:45     removed a line
+ Sruthi179            1.1             2025-02-12 14:50:14     removed two line
 */
 
 
 
 select b from a;
 select * from x;
-
