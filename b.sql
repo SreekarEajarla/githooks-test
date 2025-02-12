442c10dd2139dccdfa7d247ccb0bae840e4574eb
Sruthi179 <sruthi.eluru@bilvantis.io>
2025-02-12 12:33:28 +0530

    sql file

--- b.sql
+++ b.sql
@@ -1,20 +1,8 @@
 /* creator name: Sruthi179
- creation time: 2025-02-12 12:31:26
+ creation time: 2025-02-12 12:33:28
  Modified by         Version          Modified timestamp      Commit message
  --------------------------------------------------------------------------------
- Sruthi179            1.0             2025-02-12 12:31:26      sql file
+ Sruthi179            1.0             2025-02-12 12:33:28      sql file
 */
-
-/* Sruthi179            1.0             2025-02-12 12:30:41     changed sql file
-
- creation time: 2025-02-12 12:29:33
- Modified by         Version          Modified timestamp      Commit message
- --------------------------------------------------------------------------------
- Sruthi179            1.0             2025-02-12 12:29:33     a new line is added
-*/
-
-/* Sruthi179            1.0             2025-02-12 12:24:24     a new line */
-
-
 
 select a from a;
