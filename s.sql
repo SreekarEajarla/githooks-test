71a686784da2c171fad6e94d1f813dd21a9ce2e6
Sruthi179 <sruthi.eluru@bilvantis.io>
2025-02-12 15:01:56 +0530

    qwerty

---
+++ s.sql
@@ -0,0 +1,8 @@
+/* creator name: Sruthi179
+ creation time: 2025-02-12 15:01:56
+ Modified by         Version          Modified timestamp      Commit message
+ --------------------------------------------------------------------------------
+ Sruthi179            1.0             2025-02-12 15:01:56     qwerty
+*/
+
+select * from a;
