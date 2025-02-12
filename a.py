67722699e2d44de8205b38cdbc1648748638334f
Sruthi179 <sruthi.eluru@bilvantis.io>
2025-02-12 12:24:24 +0530

    a new line is added

--- a.py
+++ a.py
@@ -1,7 +1,6 @@
-# creator name: Sruthi179
-# creation time: 2025-02-12 12:23:15
-# Modified by         Version          Modified timestamp      Commit message
-# --------------------------------------------------------------------------------
+# Sruthi179            1.1             2025-02-12 12:24:24     a new line is added
 # Sruthi179            1.0             2025-02-12 12:23:15     New file created
 
 print(10+5)
+print("X")
+
--- b.sql
+++ b.sql
@@ -1,8 +1,2 @@
-/* creator name: Sruthi179
- creation time: 2025-02-12 12:23:15
- Modified by         Version          Modified timestamp      Commit message
- --------------------------------------------------------------------------------
- Sruthi179            1.0             2025-02-12 12:23:15     New file created
-*/
+/* Sruthi179            1.0             2025-02-12 12:24:24     a new line is added
 
-select * from table;
