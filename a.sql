2d3cd8b57861177e585718204d85c70a4dfb68e0
Sruthi179 <sruthi.eluru@bilvantis.io>
2025-02-12 14:47:36 +0530

    asdfg

--- a.sql
+++ a.sql
@@ -5,10 +5,10 @@
  Sruthi179            1.0             2025-02-12 12:39:00     an sql file
  Sruthi179            1.0             2025-02-12 14:45:31     new line
  Sruthi179            1.0             2025-02-12 14:46:00     qwerty
+ Sruthi179            1.0             2025-02-12 14:47:36     asdfg
 */
 
 
 
 select b from a;
-
 select * from x;
