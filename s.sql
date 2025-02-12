a0b959f18a6a68e93d45c9ae18d4e36912f56e0e
Sruthi179 <sruthi.eluru@bilvantis.io>
2025-02-12 15:22:48 +0530

    aszdfghjkl

--- s.sql
+++ s.sql
@@ -1,12 +1,11 @@
-/* creator name: Sruthi179
- creation time: 2025-02-12 15:01:56
- Modified by         Version          Modified timestamp      Commit message
- --------------------------------------------------------------------------------
- Sruthi179            1.0             2025-02-12 15:01:56     qwerty
- Sruthi179            1.0             2025-02-12 15:02:29     * to sal
- Sruthi179            1.0             2025-02-12 15:10:38     * from b
- Sruthi179            1.0             2025-02-12 15:11:04     sal from b
+/*
+creator name: Sruthi179
+creation time: 2025-02-12 15:22:49
+Modified by         Version          Modified timestamp      Commit message
+--------------------------------------------------------------------------------
+Sruthi179            1.0             2025-02-12 15:22:49     aszdfghjkl
 */
+
 
 select sal from a;
 select sal from b;
