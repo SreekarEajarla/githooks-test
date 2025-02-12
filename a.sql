3684e06941b0b10ff4423a0593a5a8ff30f91269
Sruthi179 <sruthi.eluru@bilvantis.io>
2025-02-12 12:39:00 +0530

    an sql file

--- a.sql
+++ a.sql
@@ -1,9 +1,11 @@
 /* creator name: Sruthi179
- creation time: 2025-02-12 12:38:08
+ creation time: 2025-02-12 12:39:00
  Modified by         Version          Modified timestamp      Commit message
  --------------------------------------------------------------------------------
- Sruthi179            1.0             2025-02-12 12:38:08      sql file
+ Sruthi179            1.0             2025-02-12 12:39:00     an sql file
 */
 
 
+
 select b from a;
+select x from y;
