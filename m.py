3e35eb6e68990df3dc4a409123cebcec324cdf6a
Sreekar <Sreekar.Eajarla@bilvantis.io>
2025-02-25 15:17:24 +0530

    Revert "Added spaces"
    This reverts commit 24d61109da0812fd7bf2380b06285904b55a4c5b.

--- m.py
+++ m.py
@@ -2,7 +2,7 @@
 # creation time: 2025-02-10 16:57:53
 # Modified by         Version          Modified timestamp      Commit message
 # --------------------------------------------------------------------------------
-# Sreekar              2.3             2025-02-25 15:13:36     Added spaces
+# Sreekar              2.3             2025-02-25 15:17:24     PENDING_COMMIT_MESSAGE
 # Sreekar              2.2             2025-02-10 17:55:10     New commit
 # Sruthi179            2.1             2025-02-10 17:07:42     Code changes
 # Sreekar              2.0             2025-02-10 17:05:08     - all spaces
@@ -23,6 +23,4 @@ a = 2
 b = 1
 
 print(a*b)
-
-
 
