659643f652ac15040db788ca7b881b118a48e1b1
SreekarEajarla <Sreekar.Eajarla@bilvantis.io>
2025-05-09 11:46:37 +0530

    removed new lines

--- s.sql
+++ s.sql
@@ -3,9 +3,11 @@ creator name: Sruthi179
 creation time: 2025-02-12 15:22:49
 Modified by         Version          Modified timestamp      Commit message
 --------------------------------------------------------------------------------
+SreekarEajarla       1.2             2025-05-09 11:46:37     removed new lines
 Sruthi179            1.1             2025-02-12 15:23:42     * from b
 Sruthi179            1.0             2025-02-12 15:22:49     aszdfghjkl
 */
+
 
 
 
