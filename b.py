560d41f7b88736be78432d8aaa885981ff6b8b6e
SreekarEajarla <Sreekar.Eajarla@bilvantis.io>
2025-05-09 11:44:26 +0530

    New lines

--- b.py
+++ b.py
@@ -2,8 +2,12 @@
 # creation time: 2025-02-12 12:37:35
 # Modified by         Version          Modified timestamp      Commit message
 # --------------------------------------------------------------------------------
+# SreekarEajarla       1.2             2025-05-09 11:44:27     New lines
 # Sruthi179            1.1             2025-02-12 15:24:25     * from b
 # Sruthi179            1.0             2025-02-12 12:37:35      sql file
 
 
+
 print("A")
+
+
